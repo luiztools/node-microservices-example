2d03fdb74f0e924adfa2a4aab02bc3193a3b5bf8
luiztools <luizfduartejr@gmail.com>
2021-02-10 20:58:19 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-microservices-example
 A simple Node.js microservices example.
+
+Para ver o tutorial completo, acesse: https://www.luiztools.com.br/post/arquitetura-de-micro-servicos-em-node-js-mongodb/
+
+Conheça meus cursos: https://www.luiztools.com.br/meus-cursos
+
+Conheça meus livros: https://www.luiztools.com.br/meus-livros
+
+Me siga nas redes sociais: https://about.me/luiztools
---
+++ movies-service/src/server/server.js
@@ -0,0 +1,27 @@
+//server.js
+require("express-async-errors");
+const express = require('express');
+const morgan = require('morgan');
+let server = null;
+
+async function start(api, repository) {
+    const app = express();
+    app.use(morgan('dev'));
+
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.sendStatus(500);
+    })
+
+    api(app, repository);
+
+    server = app.listen(process.env.PORT);
+    return server;
+}
+
+async function stop() {
+    if (server) await server.close();
+    return true;
+}
+
+module.exports = { start, stop }
